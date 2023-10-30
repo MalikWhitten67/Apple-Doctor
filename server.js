dc28aad319edf7b5693272a605825733e964bad3
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-10-30 15:29:40 -0500

    used env variables to be more secure
    Swapped to env variables for more security, added configurability via a config.json file, added more api end points, as well as added more data to be returned.

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from 'express'
 import fetch from 'node-fetch'
 import cors from 'cors'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 import fs from 'fs'
 app.use(cors());
 const port = 3000;
 app.use(express.json());
 app.get('/', (req, res) => res.json({ status: 'online' }));
-
-let json = JSON.parse( fs.readFileSync('./data.json' , 'utf8'))
-let key = ""
+let config = JSON.parse(fs.readFileSync('./config.json', 'utf-8'))
+let json = JSON.parse(fs.readFileSync(config.dataset, 'utf-8'))
+let rated_confidence = config.rated_confidence
+let min_confidence = config.min_confidence
 let hastrained = false
+let password = process.env['admin_password']
+const key = process.env['api_key']
+
 app.post('/train', (req, res) => {
 
   let url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/train";
@@ -31,38 +41,91 @@ app.post('/train', (req, res) => {
       res.send(error);
     });
 })
-app.get('/', (req, res) => {
-  res.send('Ai Backend Working')
+
+app.get('/status', (req, res) => {
+  let url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/status"
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+
+  }).then(response => {
+    if (!response.ok) {
+      console.log('Error:', response.statusText)
+      return res.send({ error: response.statusText })
+    }
+    return response.json()
+  })
+    .then((data) => {
+      console.log(data)
+      let status = {
+        2: "Online",
+        1: "Training in Progress",
+        0: "Issue present please try again"
+      }
+      res.send({ status: status[data.status] })
+    })
+    .catch((e) => {
+      console.log(e)
+      res.send({ error: 'Invalid Key' })
+    })
 })
 
 app.post('/classify', (req, res) => {
-  let data = req.body;
-  let url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/classify";
- 
-     fetch(url, {
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-       body: JSON.stringify({ data: data.data }),
-     }).then(response => response.json())
-      .then(data => {
+  const data = req.body;
+  const url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/classify";
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ data: data.data }),
+  })
+    .then(response => response.json())
+    .then(data => {
+      let bestResult = null;
 
-         data.forEach(element => {
-           res.send(element.confidence > 60 || element.confidence < 60 && element.confidence > 20 ? {
-             illeness_or_disease:element.class_name,
-             severity:json.training_data.find(x => x.label == element.class_name).severity,
-             confidence:element.confidence,
-             description:json.training_data.find(x => x.label == element.class_name).description
-           }: `Please follow up with your doctor if it is severe - I cannot find the given illness/disease you may contain from my dataset!`)
+      data.forEach(element => {
+        const class_name = element.class_name;
+        const confidence = element.confidence;
+        const trainingData = json.training_data.find(x => x.label === class_name);
 
-         });
-       })
-       .catch((error) => {
-         console.error('I am whining', error);
-       });
-  
+        if (!bestResult || confidence > bestResult.confidence) {
+          bestResult = {
+            illness_or_disease: class_name,
+            severity: trainingData.severity,
+            confidence: confidence,
+            description: trainingData.description,
+            image: trainingData.image,
+            suggestion: trainingData.suggestion,
+            common_symptoms: trainingData.common_symptoms
+          };
+        }
+      });
+
+      if (bestResult) {
+        console.log(bestResult);
+        res.send(bestResult);
+      } else {
+        res.send({
+          error: `Please consult your doctor if it is severe. The illness/disease cannot be found in my dataset!`,
+          confidence: 0, // Set confidence to 0 or another appropriate value
+          code: 200
+        });
+      }
+    })
+    .catch((error) => {
+      console.error('An error occurred:', error);
+      res.send({ error: 'an error occurred' });
+    });
+
 });
+
+app.get('/dash', (req, res) => {
+  res.setHeader('Content-Type', 'text/html')
+  res.sendFile(path.join(__dirname + '/admin_dash/dash.html'))
+})
 
 app.listen(port, () => console.log(`Apple Doc Listening for requests on: ${port}!`));
 
@@ -78,18 +141,18 @@ function train(data, label) {
       console.log('Success:', data);
     })
 }
-function trainModel(){
- 
-     fetch(`https://machinelearningforkids.co.uk/api/scratch/${key}/models`,{
-       method: 'POST',
-       headers: {
-         'Content-Type': 'application/json',
-       },
-       body: JSON.stringify({ }),
-     }).then(response => response.json())
-       .then(data => {
-         console.log('Success:', data);
-       })
+function trainModel() {
+
+  fetch(`https://machinelearningforkids.co.uk/api/scratch/${key}/models`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({}),
+  }).then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
 }
 function classify(data) {
   fetch('http://localhost:3000/classify', {
@@ -104,7 +167,7 @@ function classify(data) {
     })
 }
 
-function persistBulkData(){
+function persistBulkData() {
   for (var i in json.training_data) {
     let symptoms = json.training_data[i].symptoms;
     let label = json.training_data[i].label;
