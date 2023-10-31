98500851c92ca11cd7802fa040e60d5d7a1dd977
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2023-10-30 22:45:16 -0500

    Added the ability to bulk edit labels and add data recursively - also added error handling
    When there's a lot of data it can be tiring going 1 by one this does it for you

--- server.js
+++ server.js
@@ -21,55 +21,13 @@ let hastrained = false
 let password = process.env['admin_password']
 const key = process.env['api_key']
 
-app.post('/train', (req, res) => {
-
-  let url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/train";
+app.post('/train', async (req, res) => {
   let data = req.body;
-  fetch(url, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ data: data.data, label: data.label }),
-  }).then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-      res.send(data);
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-      res.send(error);
-    });
+  res.json(await train(data.data,data.label))
 })
 
-app.get('/status', (req, res) => {
-  let url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/status"
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-
-  }).then(response => {
-    if (!response.ok) {
-      console.log('Error:', response.statusText)
-      return res.send({ error: response.statusText })
-    }
-    return response.json()
-  })
-    .then((data) => {
-      console.log(data)
-      let status = {
-        2: "Online",
-        1: "Training in Progress",
-        0: "Issue present please try again"
-      }
-      res.send({ status: status[data.status] })
-    })
-    .catch((e) => {
-      console.log(e)
-      res.send({ error: 'Invalid Key' })
-    })
+app.get('/status', async (req, res) => {
+  res.json(await getStatus())
 })
 
 app.post('/classify', (req, res) => {
@@ -80,7 +38,7 @@ app.post('/classify', (req, res) => {
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ data: data.data }),
+    body: JSON.stringify({ data: data }),
   })
     .then(response => response.json())
     .then(data => {
@@ -127,20 +85,75 @@ app.get('/dash', (req, res) => {
   res.sendFile(path.join(__dirname + '/admin_dash/dash.html'))
 })
 
+app.get('/bulktrain', async (req, res) => {
+
+  res.json(await persistBulkData())
+
+})
 app.listen(port, () => console.log(`Apple Doc Listening for requests on: ${port}!`));
 
 function train(data, label) {
-  fetch('http://localhost:3000/train', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ data: data, label: label }),
-  }).then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
+  return new Promise((resolve, reject) => {
+    let url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/train";
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data: data, label: label }),
     })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+        resolve(data);
+      })
+      .catch((e)=>{
+        reject(e)
+      })
+  })
+  
 }
+function getStatus() {
+  return new Promise((resolve, reject) => {
+    let url = "https://machinelearningforkids.co.uk/api/scratch/" + key + "/status";
+
+    fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => {
+        if (!response.ok) {
+          console.log('Error:', response.statusText);
+          reject({ error: response.statusText });
+        }
+        return response.json();
+      })
+      .then(data => {
+        let status = {
+          2: {
+            code: 200,
+            indicator: 'Online - ready to use'
+          },
+          1: {
+            code: 400,
+            indicator: 'Training - not ready to use'
+          },
+          0: {
+            code: 500,
+            indicator: 'An error occured'
+          },
+        };
+        resolve({ status: status[data.status].code, indicator:status[data.status].indicator });
+      })
+      .catch(error => {
+        console.log(error);
+        reject({ error: 'Invalid Key' });
+      });
+  });
+}
+
 function trainModel() {
 
   fetch(`https://machinelearningforkids.co.uk/api/scratch/${key}/models`, {
@@ -150,39 +163,42 @@ function trainModel() {
     },
     body: JSON.stringify({}),
   }).then(response => response.json())
-    .then(data => {
-      console.log('Success:', data);
-    })
-}
-function classify(data) {
-  fetch('http://localhost:3000/classify', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ data: data }),
-  }).then(response => response.text())
-    .then(data => {
-      console.log('Success:', data);
-    })
+
 }
 
-function persistBulkData() {
-  for (var i in json.training_data) {
-    let symptoms = json.training_data[i].symptoms;
-    let label = json.training_data[i].label;
+async function persistBulkData() {
+  let isOk = false;
+  try {
+    const status = await getStatus()
 
-    // Initialize a unique label for the parent
-    let parentLabel = label;
-
-    // Loop through symptoms and create unique labels
-    for (let j = 0; j < symptoms.length; j++) {
-      const symptom = symptoms[j];
-
-
-      // Train the symptom with its unique label
-      train(symptom, label)
+    if (status.status === 400) {
+      console.log('training')
+      return { status: 400, msg: 'AI is already being trained. Please wait.' }
+    } else if (status === 500) {
+      return { status: 500, msg: 'An error has occurred.' }
+    } else {
+      console.log('ready to bulk edit')
     }
-  }
 
+    for (let i in json.training_data) {
+      let symptoms = json.training_data[i].symptoms;
+      let label = json.training_data[i].label;
+
+      // Initialize a unique label for the parent
+      let parentLabel = label;
+
+      // Loop through symptoms and create unique labels
+      for (let j = 0; j < symptoms.length; j++) {
+        const symptom = symptoms[j];
+
+        // Train the symptom with its unique label
+        isOk = await train(symptom, label);
+      }
+    }
+    await trainModel();
+    return { status: 200, msg: 'successfully bulk trained the ai model' }
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return { status: 501, msg: 'An error has occurred.' }
+  }
 }
